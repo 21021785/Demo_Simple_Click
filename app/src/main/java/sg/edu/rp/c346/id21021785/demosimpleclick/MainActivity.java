0bb9d105eced87bd2a6d6ecd196ce0c8ba6300d8
21021785 <21021785@myrp.edu.sg>
2022-05-06 11:27:01 +0800

    First commit

---
+++ app/src/main/java/sg/edu/rp/c346/id21021785/demosimpleclick/MainActivity.java
@@ -0,0 +1,34 @@
+package sg.edu.rp.c346.id21021785.demosimpleclick;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Step 2: Create object
+    Button btn;
+    EditText name;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Step 3: Link object to UI widget
+        btn = findViewById(R.id.button1);
+        name = findViewById(R.id.editTextTextPersonName1);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, name.getText(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+}
